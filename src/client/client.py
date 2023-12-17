7a7f79f5e25df052805a19839c8a525ff9fb509c
Mirroray41 <87991171+Mirroray41@users.noreply.github.com>
2023-12-17 11:38:08 +0100

    commit

--- src/client/client.py
+++ src/client/client.py
@@ -1,6 +1,5 @@
 import socket
 
-
 class Client:
 
     def __init__(self):
@@ -14,21 +13,18 @@ class Client:
             self.__host = input("Enter Host: ")
             self.__host = self.__host if self.__host != 'localhost' else socket.gethostname()
             self.__port = input("Enter Port: ")
-            self.__port = int(self.__port if self.__port != '' else 5000)
+            self.__port = 5000
             try:
                 self.__client_socket.connect((self.__host, self.__port))
                 connecting = False
             except socket.error as e:
                 print("\x1b[0;31;40m" + "Failed to connect to server: " + str(e) + "\x1b[0m")
 
-        
-        
 
     def go(self):
 
-        self.__message = input("Enter Message: ") 
-
-        while self.__message.lower().strip() != '.logout':
+        while True:
+            self.__message = input("Enter Message: ") 
             self.__client_socket.send(self.__message.encode())
             self.__data = self.__client_socket.recv(1024).decode()
             print("Server: " + self.__data)
